9ff19ca53f546ba961f5da70e544472c43959e0e
Sneha539 <sneha.singh_cs.aiml24@gla.ac.in>
2026-02-27 14:37:48 +0530

    initialized values

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,16 @@
 /**
  * @file main.ino
  * @brief Embedded Light Intensity Monitoring using LDR
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author Sneha Singh
+ * @date 2026-02-27
  *
  * @details
  * Reads analog brightness data from LDR sensor
  * and displays structured output via Serial Monitor.
  */
 
- // TODO 1:
- // Define LDR analog pin (Use A0)
-
- // TODO 2:
- // Create variable to store sensor reading
+const int ldrPin = A0;
+int ldrValue = 0;
 
 void setup() {
 
